ed52e32cf7b79178bcc91b5ac15ec4855c37d3c3
aj40 <alexanderchristopherjohnson@gmail.com>
2022-06-03 15:32:06 -0700

    added some fx:ids, renamed the main file, and added a public link to sql db

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.recipefinder {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens com.example.recipefinder to javafx.fxml;
     exports com.example.recipefinder;
 }
