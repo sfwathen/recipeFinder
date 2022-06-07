c72e2698df4858e588d55562e28942b0a71b311f
aj40 <alexanderchristopherjohnson@gmail.com>
2022-06-07 14:00:00 -0700

    search works but only displays the dish name right now, need to add link or button

--- src/main/java/com/example/recipefinder/SearchController.java
+++ src/main/java/com/example/recipefinder/SearchController.java
@@ -5,6 +5,8 @@ import com.mysql.cj.conf.StringProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +17,10 @@ import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class SearchController implements Initializable {
@@ -27,17 +33,52 @@ public class SearchController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
+        try {
+            Statement temp_table = RecipeFinder.conn.createStatement();
+            temp_table.execute("create table if not exists searchlist"+ RecipeFinder.currentUser +" (ingredient varchar(50) primary key);");
 
-        ListView<Item> listView = new ListView<>();
-        for (int i=1; i<=20; i++) {
-            Item item = new Item("Item "+i, false);
+            Statement search = RecipeFinder.conn.createStatement();
+            ResultSet rs = search.executeQuery("select ingredient from recipe;");
 
-            // observe item's on property and display message if it changes:
-            item.onProperty().addListener((obs, wasOn, isNowOn) -> {
-                System.out.println(item.getName() + " changed on state from "+wasOn+" to "+isNowOn);
-            });
+            while (rs.next()){
+                Item item = new Item(rs.getString("ingredient"), false);
 
-            listView.getItems().add(item);
+                // observe item's on property and display message if it changes:
+                item.onProperty().addListener((obs, wasOn, isNowOn) -> {
+                    try {
+                        Statement addOrRemove = RecipeFinder.conn.createStatement();
+                        if (wasOn == false){
+                            addOrRemove.execute("insert into searchlist" + RecipeFinder.currentUser +"(ingredient) values (\"" + item.getName() +"\");");
+                        }
+                        else{
+                            addOrRemove.execute("delete from searchlist"+ RecipeFinder.currentUser +" where ingredient = \"" + item.getName() + "\"");
+                        }
+
+                        ObservableList<String> results = FXCollections.observableArrayList();
+
+                        Statement fetchResults = RecipeFinder.conn.createStatement();
+                        ResultSet resultSet = fetchResults.executeQuery("select * from dish d where d.dishID not " +
+                                "in(select dID from recipe r " +
+                                "where r.ingredient not in (select ingredient from searchlist" + RecipeFinder.currentUser +"));");
+                        while(resultSet.next()){
+                            results.add(resultSet.getString("dishName"));
+                            System.out.println("found dish: " + resultSet.getString("dishName"));
+                        }
+
+                        resultsListView.setItems(results);
+
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(item.getName() + " changed on state from "+wasOn+" to "+isNowOn);
+                });
+                ingredientListView.getItems().add(item);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
         ingredientListView.setCellFactory(CheckBoxListCell.forListView(new Callback<Item, ObservableValue<Boolean>>() {
@@ -49,7 +90,9 @@ public class SearchController implements Initializable {
 
     }
 
-    public void goToHome(ActionEvent actionEvent) throws IOException {
+    public void goToHome(ActionEvent actionEvent) throws IOException, SQLException {
+        Statement drop_table = RecipeFinder.conn.createStatement();
+        drop_table.execute("drop table if exists searchlist"+ RecipeFinder.currentUser);
         RecipeFinder.navigateToNewPage("main-menu-view.fxml");
     }
 }
