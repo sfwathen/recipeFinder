77b8f2f47c3926e537d6d32b2afbe0b48a13786e
aj40 <alexanderchristopherjohnson@gmail.com>
2022-06-07 15:44:13 -0700

    update dish for saving

--- src/main/java/com/example/recipefinder/DishController.java
+++ src/main/java/com/example/recipefinder/DishController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
@@ -26,6 +27,9 @@ public class DishController implements Initializable{
     @FXML
     private ListView ingredientListView;
 
+    @FXML
+    private Button saveButton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources){
         try {
@@ -44,6 +48,15 @@ public class DishController implements Initializable{
 
             dishNameLabel.setText(dishName);
             dishDescLabel.setText(dishDesc);
+
+            prepSt = RecipeFinder.conn.prepareStatement("select savedID from saved where savedUser = ?");
+            prepSt.setString(1, RecipeFinder.currentUser);
+            ResultSet saved = prepSt.executeQuery();
+            while(saved.next()){
+                if(saved.getInt("savedID") == RecipeFinder.currentDishID){
+                    saveButton.setText("Saved!");
+                }
+            }
 
             prepSt = RecipeFinder.conn.prepareStatement("select * from recipe where dID = ?");
             prepSt.setInt(1, RecipeFinder.currentDishID);
@@ -66,5 +79,14 @@ public class DishController implements Initializable{
 
     public void goToPrevious() throws IOException {
         RecipeFinder.navigateToNewPage(RecipeFinder.previousView);
+    }
+
+    public void addToSaved(ActionEvent actionEvent) throws Exception{
+        saveButton.setText("Saved!");
+        PreparedStatement prepSt = RecipeFinder.conn.prepareStatement("insert into saved(savedUser, savedID) values (?,?)");
+        prepSt.setString(1, RecipeFinder.currentUser);
+        prepSt.setInt(2, RecipeFinder.currentDishID);
+        prepSt.executeUpdate();
+
     }
 }
