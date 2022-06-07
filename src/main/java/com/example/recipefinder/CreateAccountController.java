c1a17d97effff6dd8d74bcb93fed8a6444f10aac
Dean Sadaoka <dsadaoka@calpoly.edu>
2022-06-07 00:09:01 -0700

    Login and Create Account Finished

--- src/main/java/com/example/recipefinder/CreateAccountController.java
+++ src/main/java/com/example/recipefinder/CreateAccountController.java
@@ -1,27 +1,57 @@
 package com.example.recipefinder;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
+import java.io.IOException;
 import java.sql.PreparedStatement;
 
 public class CreateAccountController {
+    @FXML private TextField createUsernameInput;
+    @FXML private PasswordField createPasswordInput;
+    @FXML private Label createErrorLabel;
+
+    @FXML protected void initialize() {
+        createErrorLabel.setTextFill(Color.color(1, 0, 0));
+    }
     @FXML
     public void createUser() {
-        String username = "Tiger1";
-        String password = "tigerPass";
+        String username = createUsernameInput.getText();
+        String password = createPasswordInput.getText();
+
+        if (username.length() < 2) {
+            createErrorLabel.setText("Username must be at least 2 characters");
+            return;
+        }
+
+        if (password.length() < 5) {
+            createErrorLabel.setText("Password must be at least 5 characters");
+            return;
+        }
 
         try {
             String insertSQL = "INSERT INTO users (username, password) VALUES (?, ?)";
             PreparedStatement preparedStatement = RecipeFinder.conn.prepareStatement(insertSQL);
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, password);
-            preparedStatement.executeQuery();
+            preparedStatement.executeUpdate();
 
             RecipeFinder.currentUser = username;
             System.out.println("User Successfully Created");
+            RecipeFinder.navigateToNewPage("main-menu-view.fxml");
         }
         catch (Exception e) {
+            System.out.println(e.getMessage());
             System.out.println("User already exists");
+            createErrorLabel.setText("User already exists");
         }
+    }
+
+    @FXML
+    protected void goBackToLogin() throws IOException {
+        RecipeFinder.navigateToNewPage("login-view.fxml");
     }
 }
--- src/main/java/com/example/recipefinder/LoginController.java
+++ src/main/java/com/example/recipefinder/LoginController.java
@@ -1,19 +1,40 @@
 package com.example.recipefinder;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class LoginController {
+    @FXML private TextField usernameInput;
+    @FXML private PasswordField passwordInput;
+    @FXML private Label loginErrorLabel;
+
+    @FXML protected void initialize() {
+        loginErrorLabel.setTextFill(Color.color(1, 0, 0));
+    }
+
     @FXML
     protected void userLogin() throws IOException {
-        String username = "Dean12";
-        String passAttempt = "password";
+        String username = usernameInput.getText();
+        String passAttempt = passwordInput.getText();
 
-        TextField usernameInput = new TextField();
+        if (username.length() <= 1) {
+            System.out.println("Username must be at least 2 characters");
+            loginErrorLabel.setText("Username must be at least 2 characters");
+            return;
+        }
+        else if (passAttempt.length() < 5) {
+            System.out.println("Password must be at least 5 characters");
+            loginErrorLabel.setText("Password must be at least 5 characters");
+            return;
+        }
+
 
         try {
             String selectSQL = "SELECT password FROM users WHERE username=?";
@@ -28,13 +49,16 @@ public class LoginController {
                 if (passActual.equals(passAttempt)) {
                     System.out.println("Successful Login");
                     RecipeFinder.currentUser = username;
+                    RecipeFinder.navigateToNewPage("main-menu-view.fxml");
                 }
                 else {
                     System.out.println("Incorrect Password");
+                    loginErrorLabel.setText("Incorrect Password");
                 }
             }
             catch (Exception e) {
                 System.out.println("User does not exist");
+                loginErrorLabel.setText("User does not exist");
             }
         }
         catch (Exception e) {
