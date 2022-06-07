2cf179e8a99c8faeb35667ae3a1daa699a0f6e4a
aj40 <alexanderchristopherjohnson@gmail.com>
2022-06-07 14:54:18 -0700

    added HBoxCell

---
+++ src/main/java/com/example/recipefinder/HBoxCell.java
@@ -0,0 +1,31 @@
+package com.example.recipefinder;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+import java.sql.Statement;
+
+public class HBoxCell extends HBox {
+    Button button = new Button();
+
+    HBoxCell(String buttonText, int dishID) {
+        super();
+        button.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(button, Priority.ALWAYS);
+
+        button.setText(buttonText);
+        button.setOnAction(e -> {
+            try {
+                Statement drop_table = RecipeFinder.conn.createStatement();
+                drop_table.execute("drop table if exists searchlist" + RecipeFinder.currentUser);
+                RecipeFinder.previousView = "search-view.fxml";
+                RecipeFinder.navigateToNewPage("dish-view.fxml", dishID);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        this.getChildren().addAll(button);
+    }
+}
--- src/main/java/com/example/recipefinder/SearchController.java
+++ src/main/java/com/example/recipefinder/SearchController.java
@@ -19,30 +19,6 @@ import java.util.ResourceBundle;
 
 public class SearchController implements Initializable {
 
-    public static class HBoxCell extends HBox{
-        Button button = new Button();
-
-        HBoxCell(String buttonText, int dishID) {
-            super();
-            button.setMaxWidth(Double.MAX_VALUE);
-            HBox.setHgrow(button, Priority.ALWAYS);
-
-            button.setText(buttonText);
-            button.setOnAction(e -> {
-                try {
-                    Statement drop_table = RecipeFinder.conn.createStatement();
-                    drop_table.execute("drop table if exists searchlist"+ RecipeFinder.currentUser);
-                    RecipeFinder.previousView = "search-view.fxml";
-                    RecipeFinder.navigateToNewPage("dish-view.fxml", dishID);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            });
-
-            this.getChildren().addAll(button);
-        }
-    }
-
     @FXML
     private ListView ingredientListView;
 
